1d9acaf45397450f24c996dd041ebebadfcd7316
Sergio <Sergio@Sergio>
2014-09-26 16:47:58 +0200

    commit inicial

---
+++ src/asix/m03/ASIXM03.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package asix.m03;
+
+/**
+ *
+ * @author Sergio
+ */
+public class ASIXM03 { 
+   
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int n1 = 4;
+        int n2 = 5;
+        
+        int resultado = n1 + n2;
+        
+        
+        System.out.println("el resultado es " + resultado);
+    
+        }
+}
